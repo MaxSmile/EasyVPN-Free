59c84a08730d732ec87e15390fd8104d51870542
Vasilkoff <dev.vasilkoff@gmail.com>
2016-11-02 19:18:42 +0200

    version name in the app

--- Android-code/app/src/main/java/com/vasilkoff/easyvpnfree/activity/AboutActivity.java
+++ Android-code/app/src/main/java/com/vasilkoff/easyvpnfree/activity/AboutActivity.java
@@ -1,7 +1,11 @@
 package com.vasilkoff.easyvpnfree.activity;
 
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import com.vasilkoff.easyvpnfree.R;
 
 public class AboutActivity extends BaseActivity {
@@ -10,5 +14,24 @@ public class AboutActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        try {
+            PackageInfo pinfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            int versionNumber = pinfo.versionCode;
+            String versionName = pinfo.versionName;
+            String appName =  AboutActivity.this.getString(R.string.app_name);
+            TextView versionText = (TextView)findViewById(R.id.appVersion);
+            versionText.setText(
+                    String.format("%s version %s build %d",
+                            appName,
+                            versionName,
+                            versionNumber
+                            )
+            );
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 }
