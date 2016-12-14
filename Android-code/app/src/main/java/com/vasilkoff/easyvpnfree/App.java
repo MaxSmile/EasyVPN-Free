90a61f244715226c295559bd175602c7b73036f1
Volodymyr Kusenko <volodymyr.kusenko@gmail.com>
2016-12-14 09:46:01 +0200

    Added Crashlytics

--- Android-code/app/src/main/java/com/vasilkoff/easyvpnfree/App.java
+++ Android-code/app/src/main/java/com/vasilkoff/easyvpnfree/App.java
@@ -2,6 +2,8 @@ package com.vasilkoff.easyvpnfree;
 
 import android.app.Application;
 import android.content.Context;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class App extends Application {
 
@@ -10,6 +12,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         instance = this;
     }
 
